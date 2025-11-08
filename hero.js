33deec6d8afc234652ad2b662e08c310aad20f88
Monir <monirislam9498@gmail.com>
2025-11-08 20:23:46 +0600

    conditional practice in programming hero

---
+++ hero.js
@@ -0,0 +1,78 @@
+// var x = "monir";
+// console.log(x);
+// var name = "sadurddin";
+// console.log(name)
+//  var isRich = true;
+//  console.log(typeof isRich)
+
+//  var applePrice = 20 ;
+//  var goavaPrice = parseInt('30') ;
+
+//  console.log(applePrice);
+//  console.log(goavaPrice);
+//  console.log( applePrice + goavaPrice);
+
+//  var price1 = 30 ;
+//  var price2 = 20 ;
+//  var totalPrice = price1 + price2 ;
+//  console.log(totalPrice)
+
+//  let apple1 = 50 ;
+//  let apple2 = 10 ;
+//  let totalApple = apple1 - apple2 ;
+//  console.log (totalApple);
+
+//  let ban1 = 30 ;
+//  let ban2 = 10 ;
+//  let totalBan = ban1 * ban2 ;
+//  console.log(totalBan);
+
+//  let man1 = 100 ;
+//  let man2 = 25 ;
+//  let allMan = man1 / man2 ;
+//  console.log (allMan);
+
+//  let modulo = 100 ;
+//  let modulo2 = 22 ;
+//  let allMo = modulo % modulo2;
+//  console.log (allMo)
+
+//  let gun1 = 22 ;
+//  let gun2 = 4 ;
+//  let gun = gun1 * gun2 ;
+//  console.log (gun);
+
+//  let biyog1 = 100 ;
+//  let biyog2 = 88 ;
+//  let biyog = biyog1 - biyog2 ;
+//  console.log (biyog);
+
+//  let x = 22000 ;
+//  let y = 80 ;
+//  let z = x * y ;
+//  console.log(z)
+
+
+ /*
+
+* Greater than : >
+* less than  : <
+* greater than or equal = >=
+* less than or equal : <=
+* equal : ==
+* not equal : !=
+*  and : &&
+* or : ||
+
+
+  */
+
+
+let price = 250 ;
+if( price < 300 ){
+   console.log( 'Give me');
+}   
+
+if (13 < 10) {
+  console.log( is it ok);
+}
